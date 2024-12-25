2c0c7cb7e6e8e725ec5175165fb954d5e4bf166c
nkglnoly <glnoly866@gmail.com>
2024-12-26 01:19:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ def communicate():
 
 
 # ユーザーインターフェイスの構築
-st.title("My AI Assistant")
-st.write("ChatGPT APIを使ったチャットボットです。")
+st.title("FuNak AI Assistant")
+st.write("ChatGPT APIを使ったチャットボットです。私が作った環境です")
 
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
 
